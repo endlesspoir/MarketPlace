3ad226567fd74251ee04e907664ba47f3134a30e
Endlesspoir <artgach@gmai.com>
2026-02-24 12:25:55 +0300

    feat(api-gateway/user-service):set base property

--- backend/api-gateway/src/main/java/com/marketplace/gateway/config/RouteConfig.java
+++ backend/api-gateway/src/main/java/com/marketplace/gateway/config/RouteConfig.java
@@ -1,5 +1,6 @@
 package com.marketplace.gateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,11 +10,12 @@ import org.springframework.context.annotation.Configuration;
 public class RouteConfig {
 
     @Bean
-    public RouteLocator routes(RouteLocatorBuilder builder) {
+    public RouteLocator routes(RouteLocatorBuilder builder,
+                               @Value("${user-service.url}") String userServiceUrl) {
         return builder.routes()
                 .route("user-service-auth", r -> r
                         .path("/api/auth/**")
-                        .uri("http://user-service"))
+                        .uri(userServiceUrl))
                 .build();
     }
 }
